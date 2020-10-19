1ec966a8aba15f994240257537ccb0b7991cbeda
Veselin Penev <penev.veselin@gmail.com>
2020-10-19 21:25:41 +0200

    fix in welcome screen

--- components/main_window.py
+++ components/main_window.py
@@ -13,7 +13,7 @@ kv = """
     Label:
         color: 0, 0, 0, 1 
         markup: True
-        text: '[size=36]Regulatory\nCompliant\nTrader[/size]'
+        text: '[size=36]Regulatory Compliant Trader[/size]'
 
 
 <ScreenManagement>:
