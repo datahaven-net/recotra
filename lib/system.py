edc474659ed85251d8196c59255c08641e54999c
Veselin Penev <penev.veselin@gmail.com>
2020-10-22 22:30:45 +0200

    make possible to print user ID card

--- lib/system.py
+++ lib/system.py
@@ -3,10 +3,41 @@ import subprocess
 
 #------------------------------------------------------------------------------
 
-def open_file(path):
+def open_system_explorer(path):
+    """
+    Simple and portable way to show location or file on local disk to the user.
+    """
     if platform.system() == "Windows":
         os.startfile(path)  # @UndefinedVariable
     elif platform.system() == "Darwin":
-        subprocess.Popen(["open", path])
+        subprocess.Popen(["open", "-R", path])
     else:
         subprocess.Popen(["xdg-open", path])
+
+#------------------------------------------------------------------------------
+
+def copy_xclip(text, primary=False):
+    DEFAULT_SELECTION='c'
+    PRIMARY_SELECTION='p'
+    ENCODING = 'utf-8'
+    selection=DEFAULT_SELECTION
+    if primary:
+        selection=PRIMARY_SELECTION
+    p = subprocess.Popen(['xclip', '-selection', selection], stdin=subprocess.PIPE, close_fds=True)
+    return p.communicate(input=text.encode(ENCODING))
+
+
+def paste_xclip(primary=False):
+    DEFAULT_SELECTION='c'
+    PRIMARY_SELECTION='p'
+    ENCODING = 'utf-8'
+    selection=DEFAULT_SELECTION
+    if primary:
+        selection=PRIMARY_SELECTION
+    p = subprocess.Popen(['xclip', '-selection', selection, '-o'],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         close_fds=True)
+    stdout, stderr = p.communicate()
+    # Intentionally ignore extraneous output on stderr when clipboard is empty
+    return stdout.decode(ENCODING)
